5867acbb1d4d55777df4b8da7a64b4f4debf03ca
thomas <thomas@localhost.localdomain>
2016-02-02 13:23:16 +0300

    Habraparser is parser of habrhabr,geektimes and megamozg

---
+++ workingwithtexts/habraparser/habraparser.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+import sys
+
+def findtag(texthtml,tag):
+    return texthtml.find(tag)
+
+def removetags(somestrin):
+    fg=open(".listoftags","r")
+    tags=fg.read().split()
+    fg.close()
+    for x in tags:
+        somestrin=somestrin.replace(x," ")
+    somestrin=somestrin.replace("<br>","\n")
+    return somestrin
+
+def removeimages(somestrin):
+    #while somestrin.find("<img src=")!=-1:
+        #somestrin=somestrin[:somestrin.find("<img src=")]+somestrin[somestrin.find(""""alt="image">""")+len(""""alt="image">"""):]
+    #while somestrin.find("<img src")!=-1:
+    #    somestrin=somestrin.replace(somestrin[somestrin.find("<img src"):somestrin.find("""alt="image">""")+len("""alt="image">""")],"image")
+    return somestrin
+
+def removelinks(somestrin):
+    #while somestrin.find("href=")!=-1:
+    #    somestrin=somestrin.replace(somestrin[somestrin.find("href="):somestrin.find("""">""")+len("""">""")]," ")
+    return somestrin
+
+def removethis(somestrin,tag):
+    for x in range(len(somestrin)):
+        if somestrin[x:x+len(tag)]==tag:
+            i=0
+            while somestrin[x+i]!=">":
+                i+=1
+            somestrin=somestrin.replace(somestrin[x:x+i+1]," ")
+    return somestrin
+
+def deletetrash(texthtml):
+    newstr=texthtml[findtag(texthtml,"""<span class="post_title">"""):]
+    newstr=newstr[:findtag(newstr,"""<div class="clear">""")]
+    newstr=newstr[:findtag(newstr,"""<div class="hubs">""")]+newstr[findtag(newstr,"""<div class="content html_format">"""):]
+    #newstr=newstr[:findtag(newstr,"<ul>")]
+    #newstr=newstr[:findtag(newstr,"<hr>")]
+    newstr=removetags(newstr)
+    #newstr=removeimages(newstr)
+    #newstr=removelinks(newstr)
+    newstr=removethis(newstr,"<img src=")
+    newstr=removethis(newstr,"href=")
+    newstr=removethis(newstr,"class=")
+    newstr=newstr.replace("<b","")
+
+
+    return newstr
+
+
+def main():
+    if len(sys.argv)!=2:
+        print("File *.html is needed to parse.")
+        sys.exit(1)
+
+    f=open(sys.argv[1],"r")
+    toparse=f.read()
+    f.close()
+    g=open(sys.argv[1]+".parsed","w")
+    g.write(deletetrash(toparse))
+    g.close()
+main()
