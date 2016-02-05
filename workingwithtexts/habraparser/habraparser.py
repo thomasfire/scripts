bb169f48a84c3e18751c11003f7ebc1ed51328ee
thomas <thomas@localhost.localdomain>
2016-02-05 22:09:24 +0300

    Added url support.Now it parses from URL

--- workingwithtexts/habraparser/habraparser.py
+++ workingwithtexts/habraparser/habraparser.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
-#this script converts *.html file of articles to text of this articles
+#this script gets article from URL
 
 import sys
-
+import urllib
+import urllib.request
+import re
+import os
+import shutil
 
 def findtag(texthtml,tag):
     return texthtml.find(tag)
@@ -16,7 +20,7 @@ def removetags(somestrin):
         somestrin=somestrin.replace(x," ")
     somestrin=somestrin.replace("<br>","\n")
     return somestrin
-    
+
 #This is bad functions,they work badly
 
 #def removeimages(somestrin):
@@ -39,29 +43,65 @@ def removethis(somestrin,tag):
             somestrin=somestrin.replace(somestrin[x:x+i+1]," ")
     return somestrin
 
-def deletetrash(texthtml):
+def findimg(texthtml):
+    a=re.compile(r"""<img.*?src="(.+?)".*?>""", re.DOTALL)
+    listofimg=re.findall(a,texthtml)
+    return listofimg
+
+def deletetrash(texthtml,num):
     newstr=texthtml[findtag(texthtml,"""<span class="post_title">"""):]
     newstr=newstr[:findtag(newstr,"""<div class="clear">""")]
     newstr=newstr[:findtag(newstr,"""<div class="hubs">""")]+newstr[findtag(newstr,"""<div class="content html_format">"""):]
     newstr=removetags(newstr)
-    newstr=removethis(newstr,"<img src=")
-    newstr=removethis(newstr,"href=")
     newstr=removethis(newstr,"class=")
     newstr=newstr.replace("<b","")
 
+    listofimg=findimg(newstr)
+    a=re.compile(r"""<img.*?src=".+?".*?>""", re.DOTALL)
+    newstr=re.sub(a," ",newstr)
 
+    b=re.compile(r"""href="(.*?)">""", re.DOTALL)
+    listoflinks=re.findall(b,newstr)
+    newstr=re.sub(b," ",newstr)
+
+    f=open(num+".images","w")
+    f.write("\n".join(listofimg))
+    f.close()
+
+    g=open(num+".links","w")
+    g.write("\n".join(listoflinks))
+    g.close()
+    #newstr=re.sub(r'\s+', ' ', newstr)
+    newstr=re.sub(r'\t+', '\n', newstr)
+    newstr=re.sub(r'\r+', '\n', newstr)
+    newstr=re.sub(r'\n+', '\n', newstr)
+    re.sub("^\s+|\n|\r|\s+$", '', newstr)
     return newstr
 
+def download_images(num):
+    f=open(num+".images","r")
+    img_urls=f.read().split()
+    if not os.path.exists(num):
+        os.mkdir(num)
+    for x in img_urls:
+        st=" ".join(list(re.findall(r"\w+?/(\w+?\.\w{3})",x)))
+        urllib.request.urlretrieve(x, num+"/"+st)
 
 def main():
     if len(sys.argv)!=2:
-        print("File *.html is needed to parse.")
+        print("URL is needed to parse.")
         sys.exit(1)
 
-    f=open(sys.argv[1],"r")
-    toparse=f.read()
+    f=urllib.request.urlopen(sys.argv[1])
+    toparse=str(f.read(),encoding='utf8')
+    a=re.compile(r"""/(\d+?)/""")
+    num=" ".join(list(re.findall(a,sys.argv[1])))
     f.close()
-    g=open(sys.argv[1]+".parsed","w")
-    g.write(deletetrash(toparse))
+    g=open(num+".parsed","w")
+    g.write(deletetrash(toparse,num))
     g.close()
+    download_images(num)
+    shutil.move(num+".parsed", num+'/'+num+".parsed")
+    shutil.move(num+".images", num+'/'+num+".images")
+    shutil.move(num+".links", num+'/'+num+".links")
 main()
