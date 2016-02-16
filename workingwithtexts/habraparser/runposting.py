b87a0417fbd6ccb3886317a97613233aa59f1170
Thomas <thomasfire@yandex.ru>
2016-02-16 17:36:12 +0300

    Added some features

--- workingwithtexts/habraparser/runposting.py
+++ workingwithtexts/habraparser/runposting.py
@@ -3,8 +3,10 @@ import vkupload as vup
 import habraparser as hper
 import parsehabrlinks as phlinks
 import makeseq as mkseq
+import shutil
 import time
 import random
+import os
 
 
 def main():
@@ -13,20 +15,36 @@ def main():
     f.close()
     links=open(resource+".links",'r').read().split()
     while True:
+        #deleting trash
+        print("Deleteing old files....")
+        dirs=os.listdir(".")
+        for x in dirs:
+            if x.isdigit():
+                shutil.rmtree(x)
+
         try:
             phlinks.parselinks(resource)
         except:
             print("Somethings wrong,maybe it's bad connection\n")
+            print("Retrying in 30 srconds...")
+            time.sleep(30)
+            continue
         try:
             for x in links:
                 hper.parsearts(x)
         except:
             print("Somethings wrong,maybe it's bad connection\n")
+            print("Retrying in 30 seconds...")
+            time.sleep(30)
+            continue
         mkseq.makeseq(resource)
         vup.startuploading(resource)
         sltime=3600 + random.randint(1,3600)
         print("Sleeping for ",sltime)
         time.sleep(sltime)
+
+
+
         print("\n")
 
 if __name__=="__main__":
